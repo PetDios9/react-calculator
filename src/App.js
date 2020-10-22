fc34a92147e96599977ba7c7c37a3ea3bf40e8e3
Peter <diosdadopeter@gmail.com>
2020-10-21 20:13:08 -0700

    added background color, header with description text

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
 import React from 'react';
 import './App.css';
 import CalcContainer from './CalcContainer';
-import {Grid, ThemeProvider, createMuiTheme } from '@material-ui/core'
-import {grey, orange } from '@material-ui/core/colors';
+import {Grid, ThemeProvider, createMuiTheme, MuiThemeProvider, CssBaseline, Typography,} from '@material-ui/core'
+import {grey, indigo, orange } from '@material-ui/core/colors';
 
 
 function App() {
 
-const theme = createMuiTheme({
+  const themeDark = createMuiTheme({
     palette: {
-        primary: {
-            main: grey[800],
-        },
-        secondary: {
-            main: orange[900]
-        }
+      background: {
+        default: "#333"
+      },
+      text: {
+        primary: "#ffffff"
+      }
+    }
+  })
+
+  const theme = createMuiTheme({
+    palette: {
+      background: {
+        default: indigo
+      },
+      primary: {
+          main: grey[800],
+      },
+      secondary: {
+          main: orange[900]
+      }
     }
   })
 
   return (
-    <ThemeProvider theme={theme}>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          justify="center"
-          alignContent="center"
-          style={{ minHeight: '100vh' }}>
+    <MuiThemeProvider theme={themeDark}>
+      <CssBaseline />
+       <ThemeProvider theme={theme}>
+          <Grid
+            container
+            spacing={8}
+            direction="column"
+            justify="center"
+            alignContent="center"
+            alignItems="center"
+            style={{ minHeight: '100vh' }}
+            >
+
+            <Grid item xs={12} >
+              <Typography variant="h2">React Calculator!</Typography>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Typography variant={"h6"} align="center">
+                This is my first time using both React and Material UI to create
+                an application. Although it took me longer than I expected and I plan on further
+                improving this app, I am looking forward to further improving my skills with both
+                frameworks on my journey to becoming a Web Developer.
+              </Typography>
+            </Grid>
           
-          <Grid item>
-            <CalcContainer />
+            <Grid item xs={12}>
+              <CalcContainer />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Typography variant={"h6"}>
+                Created By: Peter Diosdado
+              </Typography>
+            </Grid>
+          
           </Grid>
-          
-        </Grid>
-    </ThemeProvider>
+        </ ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
